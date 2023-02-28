f17ce122947432d869dfe2f10b2368f63086066a
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2023-02-28 18:04:34 -0500

    Add Stats bar under Landing page

--- src/pages/landing/landingController.js
+++ src/pages/landing/landingController.js
@@ -1,19 +1,18 @@
 import React from 'react';
-// import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import CircularProgress from '@material-ui/core/CircularProgress';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import LandingView from './landingView';
 import { Typography } from '../../components/Wrappers/Wrappers';
-// import { GET_LANDING_PAGE_DATA_QUERY } from '../../bento/landingPageData';
+import { GET_LANDING_PAGE_DATA_QUERY } from '../../bento/landingPageData';
 
 const landingController = () => {
-  const { loading, error, data } = {};
 
-  /*const isSignedIn = useSelector((state) => state.login.isSignedIn);
+  const isSignedIn = useSelector((state) => state.login.isSignedIn);
   const { loading, error, data } = useQuery(GET_LANDING_PAGE_DATA_QUERY, {
     context: { clientName: isSignedIn ? '' : 'publicService' },
     fetchPolicy: 'no-cache',
-  }); */
+  });
 
   if (loading) return <CircularProgress />;
   if (error) {
